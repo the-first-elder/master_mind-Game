3412260f6a2895224ccc9f7e702dc2cc935031e6
Samuel Dahunsi <dahunsisamuel1st@gmail.com>
2024-06-23 07:09:27 +0100

    add score and leaderboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -137,6 +137,24 @@ const App: React.FC = () => {
     return `top-[${topValue}%]`;
   };
 
+  const leaderboard = [
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      name: "Anonymous",
+      score: 0,
+    },
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      name: "Anonymous",
+      score: 0,
+    },
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      name: "Anonymous",
+      score: 0,
+    },
+  ];
+
   return (
     <div
       className="min-h-screen"
@@ -144,8 +162,8 @@ const App: React.FC = () => {
     >
       <Header />
       <div className="flex p-10 justify-center items-center min-h-screen">
-        <div className="bg-white w-fit px-20 py-10 shadow-lg rounded">
-          {isSettingSecretCode ? (
+        {isSettingSecretCode ? (
+          <div className="bg-white w-fit px-20 py-10 shadow-lg rounded">
             <div className="mb-4 flex flex-col gap-5">
               <div className="text-center mb-4">Set the Secret Code</div>
               <div className="flex items-center justify-center space-x-4">
@@ -185,94 +203,125 @@ const App: React.FC = () => {
                 Set Secret Code
               </button>
             </div>
-          ) : (
-            <div>
-              {/* Secret code row */}
-              <div className="flex items-center space-x-4 mb-4">
-                {secretCode.map((color, index) => (
-                  <div
-                    key={index}
-                    className="w-10 h-10 rounded-full"
-                    style={{
-                      backgroundColor: gameOver ? color! : "#d1d5db",
-                      border: gameOver ? `2px solid ${color}` : "none",
-                    }}
-                  ></div>
-                ))}
-              </div>
-
-              {guesses.map((guess, rowIndex) => (
-                <div key={rowIndex} className="flex items-center space-x-4 mb-4">
-                  {guess.map((color, pegIndex) => (
-                    <div key={pegIndex} className="">
-                      <div
-                        onClick={() => handlePegClick(rowIndex, pegIndex)}
-                        className={`w-10 h-10 rounded-full cursor-pointer`}
-                        style={{
-                          backgroundColor: color ? color : "#d1d5db",
-                          border: color ? `2px solid ${color}` : "none",
-                        }}
-                      ></div>
-                      {colorPicker.rowIndex === rowIndex &&
-                        colorPicker.pegIndex === pegIndex && (
-                          <div className={`absolute ${dynamicTopClassName} left-[45%] flex space-x-1 z-10 bg-white p-2 rounded shadow-lg`}>
-                            {Object.entries(COLORS).map(([key, value]) => (
-                              <div key={key}>
-                                <div
-                                  onClick={() => selectColor(value)}
-                                  className="w-8 h-8 rounded-full"
-                                  style={{
-                                    backgroundColor: value,
-                                    cursor: "pointer",
-                                  }}
-                                ></div>
-                              </div>
-                            ))}
-                          </div>
-                        )}
-                    </div>
-                  ))}
-                  <div className="flex space-x-2">
-                    {feedback[rowIndex].map((fb, fbIndex) => (
-                      <div
-                        key={fbIndex}
-                        className={`w-4 h-4 rounded-full`}
-                        style={{
-                          backgroundColor:
-                            fb === "black"
-                              ? "#000"
-                              : fb === "white"
-                                ? "#879cb0"
-                                : "#d1d5db",
-                        }}
-                      ></div>
-                    ))}
+          </div>
+        ) : (
+          <div className="flex items-start justify-center gap-20">
+            <div className="mt-5">
+              <div className="flex flex-col gap-10">
+                <div>
+                  <p className="font-bold text-xl mb-5 text-white">My Score</p>
+                  <div>
+                    <p className="text-lg text-white">45</p>
                   </div>
                 </div>
-              ))}
-              <button
-                onClick={handleCheck}
-                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-                disabled={gameOver}
-              >
-                Check
-              </button>
-              {gameOver && (
-                <div className="mt-4 text-center">
-                  {gameWon ? (
-                    <div className="text-green-500">
-                      You won! Congratulations!
+                <div>
+                  <p className="font-bold text-xl mb-5 text-white">Leaderboard</p>
+                  <div className="flex flex-col gap-5">
+                    <div
+                      className="bg-white px-5 py-3 shadow-lg rounded">
+                      {leaderboard.map((wallet, index) => (
+                        <div key={wallet.address} className="flex flex-row justify-between gap-16">
+                          <div>{index + 1}</div>
+                          <div>{wallet.name}</div>
+                          <div>{wallet.address.slice(0, 6)}...{wallet.address.slice(-4)}</div>
+                          <div>{wallet.score}</div>
+                        </div>
+                      ))}
                     </div>
-                  ) : (
-                    <div className="text-red-500">
-                      Game over! The secret code was displayed above.
-                    </div>
-                  )}
+                  </div>
                 </div>
-              )}
+              </div>
             </div>
-          )}
-        </div>
+            <div className="bg-white w-fit px-20 py-10 shadow-lg rounded">
+
+              <div>
+                {/* Secret code row */}
+                <div className="flex items-center space-x-4 mb-4">
+                  {secretCode.map((color, index) => (
+                    <div
+                      key={index}
+                      className="w-10 h-10 rounded-full"
+                      style={{
+                        backgroundColor: gameOver ? color! : "#d1d5db",
+                        border: gameOver ? `2px solid ${color}` : "none",
+                      }}
+                    ></div>
+                  ))}
+                </div>
+
+                {guesses.map((guess, rowIndex) => (
+                  <div key={rowIndex} className="flex items-center space-x-4 mb-4">
+                    {guess.map((color, pegIndex) => (
+                      <div key={pegIndex} className="">
+                        <div
+                          onClick={() => handlePegClick(rowIndex, pegIndex)}
+                          className={`w-10 h-10 rounded-full cursor-pointer`}
+                          style={{
+                            backgroundColor: color ? color : "#d1d5db",
+                            border: color ? `2px solid ${color}` : "none",
+                          }}
+                        ></div>
+                        {colorPicker.rowIndex === rowIndex &&
+                          colorPicker.pegIndex === pegIndex && (
+                            <div className={`absolute ${dynamicTopClassName} left-[60%] flex space-x-1 z-10 bg-white p-2 rounded shadow-lg`}>
+                              {Object.entries(COLORS).map(([key, value]) => (
+                                <div key={key}>
+                                  <div
+                                    onClick={() => selectColor(value)}
+                                    className="w-8 h-8 rounded-full"
+                                    style={{
+                                      backgroundColor: value,
+                                      cursor: "pointer",
+                                    }}
+                                  ></div>
+                                </div>
+                              ))}
+                            </div>
+                          )}
+                      </div>
+                    ))}
+                    <div className="flex space-x-2">
+                      {feedback[rowIndex].map((fb, fbIndex) => (
+                        <div
+                          key={fbIndex}
+                          className={`w-4 h-4 rounded-full`}
+                          style={{
+                            backgroundColor:
+                              fb === "black"
+                                ? "#000"
+                                : fb === "white"
+                                  ? "#879cb0"
+                                  : "#d1d5db",
+                          }}
+                        ></div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+                <button
+                  onClick={handleCheck}
+                  className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+                  disabled={gameOver}
+                >
+                  Check
+                </button>
+                {gameOver && (
+                  <div className="mt-4 text-center">
+                    {gameWon ? (
+                      <div className="text-green-500">
+                        You won! Congratulations!
+                      </div>
+                    ) : (
+                      <div className="text-red-500">
+                        Game over! The secret code was displayed above.
+                      </div>
+                    )}
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
